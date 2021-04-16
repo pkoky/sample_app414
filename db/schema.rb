96f73907e833ead092650fac2d22e4b0f60c4324
pkoky <sscsk2224@gmail.com>
2021-04-16 12:54:04 +0000

    [add]

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_004301) do
+ActiveRecord::Schema.define(version: 2021_04_16_123740) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "body"
     t.string "title"
+    t.string "image_id"
   end
 
 end
